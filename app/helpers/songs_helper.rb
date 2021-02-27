2c16b5d4eb0e7d58aff3a990540f75269360fd30
chrislemus <icrislemus@gmail.com>
2021-02-27 13:34:16 -0500

    commit tree 3e113cc1105e058b6dc6b260d149c4f9a7932f90

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,17 @@
 module SongsHelper
-
+  def display_artist(song)
+    artist = song.artist
+    if artist
+      link_to artist.name, artist_path(artist)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 end
+
+
+# <%if @artist%>
+#   <h2><%= link_to @artist.name, artist_path(@artist) %></h2>
+#   <%else%>
+#   <h2><%= link_to "Add Artist", edit_song_path(@song) %></h2>
+#   <%end%>
